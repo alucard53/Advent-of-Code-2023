f0e9ca7bf448a7d3546c9157fb87df6b7dfa8c27
sarthak <75154468+tru69er@users.noreply.github.com>
2023-12-09 02:32:15 +0530

    R(egret)ust outta nowhere

--- d8p1.rs
+++ d8p1.rs
@@ -1,30 +1,22 @@
 use std::collections::HashMap;
 use std::fs;
-use std::mem::drop;
-
 // removed whitespaces, brackets, equal, signs and commas from input(vim hax)
 //
 fn main() {
-    let input: Vec<String> = fs::read_to_string("input.txt")
-        .unwrap()
-        .lines()
-        .map(|a| a.to_string())
-        .collect();
+    let content = fs::read_to_string("input.txt").unwrap();
+    let input: Vec<&str> = content.lines().collect();
 
-    let dirs = input[0].clone();
+    let dirs = input[0];
 
-    let mut map: HashMap<String, [String; 2]> = HashMap::new();
+    let mut map: HashMap<&str, [&str; 2]> = HashMap::new();
 
     for i in 2..input.len() {
-        let line: Vec<String> = (&input[i]).split(" ").map(|i| i.to_string()).collect();
+        let line: Vec<&str> = (&input[i]).split(" ").collect();
 
-        map.insert(line[0].clone(), [line[1].clone(), line[2].clone()]);
+        map.insert(line[0], [line[1], line[2]]);
     }
 
-    drop(input);
-
-    let start = String::from("AAA");
-    let mut pos = &start;
+    let mut pos = "AAA";
     let mut moves: usize = 0;
 
     while pos != "ZZZ" {
