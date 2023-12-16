6c534be068dd224797cba0c3c75741c0f20e13e4
sarthak <75154468+tru69er@users.noreply.github.com>
2023-12-16 15:46:32 +0530

    Day 16

---
+++ d16p2.py
@@ -0,0 +1,89 @@
+from typing import List
+
+directions = {
+    'l': [0, -1],
+    'r': [0, 1],
+    'u': [-1, 0],
+    'd': [1, 0],
+}
+
+count = 0
+def traverse(grid: List[List[str]], vis: List[List[bool]], splits: List[List[bool]], i: int, j: int, dir: str):
+    global count
+
+    while 0 <= i and i < len(grid) and 0 <= j  and j < len(grid[0]):
+        if not vis[i][j]:
+            count += 1
+        vis[i][j] = True
+            
+        if grid[i][j] == '|' and (dir == 'l' or dir == 'r'):
+            if splits[i][j]:
+                return
+            splits[i][j] = True
+            traverse(grid, vis, splits, i - 1, j, 'u')
+            dir = 'd'
+        elif (grid[i][j] == '-' and (dir == 'u' or dir == 'd')):
+            if splits[i][j]:
+                return
+            splits[i][j] = True
+            traverse(grid, vis, splits, i, j + 1, 'r')
+            dir = 'l'
+        elif grid[i][j] == '\\':
+            match dir:
+                case 'r':
+                    dir = 'd'
+                case 'l':
+                    dir = 'u'
+                case 'u':
+                    dir = 'l'
+                case 'd':
+                    dir = 'r'
+        elif grid[i][j] == '/':
+            match dir:
+                case 'r':
+                    dir = 'u'
+                case 'l':
+                    dir = 'd'
+                case 'u':
+                    dir = 'r'
+                case 'd':
+                    dir = 'l'
+
+        i += directions[dir][0]
+        j += directions[dir][1]
+
+if __name__ == '__main__':
+    max_count = 0
+    with open('input.txt') as fin:
+        grid = [list(i) for i  in fin.read().strip().split('\n')]
+
+    for j in range(len(grid[0])):
+        vis = [[False] * len(grid[0]) for _ in range(len(grid))]
+        splits = [[False] * len(grid[0]) for _ in range(len(grid))]
+
+        traverse(grid, vis, splits, 0, j, 'd')
+        max_count = max(count, max_count)
+        count = 0
+    for i in range(len(grid[0])):
+        vis = [[False] * len(grid[0]) for _ in range(len(grid))]
+        splits = [[False] * len(grid[0]) for _ in range(len(grid))]
+
+        traverse(grid, vis, splits, i, 0, 'r')
+        max_count = max(count, max_count)
+        count = 0
+    for j in range(len(grid[0])):
+        vis = [[False] * len(grid[0]) for _ in range(len(grid))]
+        splits = [[False] * len(grid[0]) for _ in range(len(grid))]
+
+        traverse(grid, vis, splits, len(grid) - 1, j, 'u')
+        max_count = max(count, max_count)
+        count = 0
+    for i in range(len(grid[0])):
+        vis = [[False] * len(grid[0]) for _ in range(len(grid))]
+        splits = [[False] * len(grid[0]) for _ in range(len(grid))]
+
+        traverse(grid, vis, splits, i, len(grid[0]) - 1, 'l')
+        max_count = max(count, max_count)
+        count = 0
+
+    print(max_count)
